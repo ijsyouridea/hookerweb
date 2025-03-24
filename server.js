1aafdff2e1f18818e1514cb567d55698ffc4bd53
Валерій Зінченко <ijsyouridea@gmail.com>
2025-03-24 14:01:45 +0100

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 app.use(express.json());
 let notifications = [];
 // Serve static files from the 'public' folder
